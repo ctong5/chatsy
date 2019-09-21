6e31386a5beb6be4bcb8fa2c1373565b711ecd21
ctong5 <cassandra.tong5@gmail.com>
2019-09-20 17:59:55 -0700

    Style users online container

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import Avatar from '../../public/avatar.png';
 
 const Users = () => (
-  <div>
-    <p>User1</p>
-    <p>User2</p>
-    <p>User3</p>
-    <p>User4</p>
+  <div className="usersContainer">
+    <div className="usersTitle">
+      <p>Users Online</p>
+    </div>
+    <div className="userCardContainer">
+      <div className="userCard">
+        <img src={Avatar} alt="profile avatar"/>
+        <p>User1</p>
+      </div>
+
+      <div className="userCard">
+        <img src={Avatar} alt="profile avatar"/>
+        <p>User2</p>
+      </div>
+
+
+      <div className="userCard">
+        <img src={Avatar} alt="profile avatar"/>
+        <p>User3</p>
+      </div>
+
+      <div className="userCard">
+        <img src={Avatar} alt="profile avatar"/>
+        <p>User4</p>
+      </div>
+    </div>
   </div>
 );
 
