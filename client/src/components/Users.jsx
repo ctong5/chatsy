1e86d96e4820c37986ddec1dcbaf16857090abb8
ctong5 <cassandra.tong5@gmail.com>
2019-09-21 22:33:50 -0700

    Correct cards overflow in parent container

--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -3,29 +3,58 @@ import Avatar from '../../public/avatar.png';
 
 const Users = () => (
   <div className="usersContainer">
-    <div className="usersTitle">
-      <p>Users Online</p>
-    </div>
-    <div className="userCardContainer">
-      <div className="userCard">
-        <img src={Avatar} alt="profile avatar"/>
-        <p>User1</p>
-      </div>
+    <div className="usersTitle"><p>Users Online</p></div>
+    <div className="cards">
+      <div className="userCardContainer">
+        <div className="userCard">
+          <img src={Avatar} alt="profile avatar"/>
+          <p>User1</p>
+        </div>
 
-      <div className="userCard">
-        <img src={Avatar} alt="profile avatar"/>
-        <p>User2</p>
-      </div>
+        <div className="userCard">
+          <img src={Avatar} alt="profile avatar"/>
+          <p>User2</p>
+        </div>
 
+        <div className="userCard">
+          <img src={Avatar} alt="profile avatar"/>
+          <p>User3</p>
+        </div>
 
-      <div className="userCard">
-        <img src={Avatar} alt="profile avatar"/>
-        <p>User3</p>
-      </div>
+        <div className="userCard">
+          <img src={Avatar} alt="profile avatar"/>
+          <p>User4</p>
+        </div>
 
-      <div className="userCard">
-        <img src={Avatar} alt="profile avatar"/>
-        <p>User4</p>
+        <div className="userCard">
+          <img src={Avatar} alt="profile avatar"/>
+          <p>User5</p>
+        </div>
+
+        <div className="userCard">
+          <img src={Avatar} alt="profile avatar"/>
+          <p>User6</p>
+        </div>
+
+        <div className="userCard">
+          <img src={Avatar} alt="profile avatar"/>
+          <p>User7</p>
+        </div>
+
+        <div className="userCard">
+          <img src={Avatar} alt="profile avatar"/>
+          <p>User8</p>
+        </div>
+
+        <div className="userCard">
+          <img src={Avatar} alt="profile avatar"/>
+          <p>User9</p>
+        </div>
+
+        <div className="userCard">
+          <img src={Avatar} alt="profile avatar"/>
+          <p>User10</p>
+        </div>
       </div>
     </div>
   </div>
