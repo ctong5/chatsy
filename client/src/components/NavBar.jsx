24f5c952626dc7cc368f0c2ffa2ef2065fbeb080
ctong5 <cassandra.tong5@gmail.com>
2019-09-21 20:45:14 -0700

    Restyle navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -5,9 +5,14 @@ const NavBar = () => (
   <div className="containerNav">
     <div className="navList">
       <ul className="navUl">
-        <li>Home</li>
-        <li><img src={Logo} alt="chatsy logo"/></li>
-        <li>User</li>
+        <div className="navLeft">
+          <li><img src={Logo} alt="chatsy logo"/></li>
+        </div>
+        <div className="navRight">
+          <li>Home</li>
+          <li>Rooms</li>
+          <li>User</li>
+        </div>
       </ul>
     </div>
   </div>
